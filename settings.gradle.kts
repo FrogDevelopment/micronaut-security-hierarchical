29bedf66f829e33a2149f1e44ea7b2a2be415cdd
Le Gall Benoit <legall.benoit@gmail.com>
2024-08-15 23:25:35 +0200

    [micronaut] upgrade to micronaut-core 4.5.0, micronaut-gradle 4.4.2 and use micronaut-platform

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,8 @@
+plugins {
+    id("io.micronaut.platform.catalog") version "4.4.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
 rootProject.name = "micronaut-security-hierarchical"
 
 dependencyResolutionManagement {
